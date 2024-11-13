4d1f9be43b80df0f7a12af857a84d194f3b45a7a
adrianchinjen <adrian.chinjen28@gmail.com>
2024-11-13 20:31:59 +0800

    feat: added signup api

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { CreateUserDto } from './dto/createUser.dto';
+import * as bcrypt from 'bcrypt';
+import { DatabaseService } from 'src/database/database.service';
+import { user_role } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private databaseService: DatabaseService
+  ) {}
 
   async userSignUp(signUpCredentials: CreateUserDto) {
-    // const { email, username, password } = signUpCredentials;
-    return signUpCredentials;
+    const { email, password, username } = signUpCredentials;
+
+    const isEmailExisting = await this.userService.checkUserEmail(email);
+
+    if (isEmailExisting) {
+      throw new ConflictException('User already exist');
+    }
+
+    const salt = await bcrypt.genSaltSync(10);
+    const hashedPassword = await bcrypt.hashSync(password, salt);
+    const authData = {
+      username,
+      email,
+      password: hashedPassword,
+      roles: [user_role.user]
+    };
+
+    const saveAuthData = await this.databaseService.auth.create({
+      data: authData
+    });
+
+    const userData = {
+      username: saveAuthData.username,
+      email: saveAuthData.email,
+      roles: saveAuthData.roles,
+      auth_id: saveAuthData.id
+    };
+
+    const saveUserInfo = await this.userService.saveUser(userData);
+
+    return saveUserInfo;
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,9 +7,16 @@ export interface AuthDetails {
   username: string;
   email: string;
   password: string;
-  role: user_role;
+  roles: user_role[];
   // created_at: string;
   // updated_at: string;
+}
+
+interface UserDetails {
+  username: string;
+  email: string;
+  roles: user_role[];
+  auth_id: number;
 }
 
 @Injectable()
@@ -17,11 +24,30 @@ export class UsersService {
   constructor(private databaseService: DatabaseService) {}
 
   async checkUserEmail(email: string) {
-    // let userExist = false;
-    const isEmailExisting: AuthDetails = await this.databaseService.auth.findUnique({
+    let userExist = false;
+    const userEmail: AuthDetails = await this.databaseService.auth.findUnique({
       where: { email }
     });
 
-    return isEmailExisting;
+    const isUserExisting = userEmail ? (userExist = true) : userExist;
+
+    return isUserExisting;
+  }
+
+  async saveUser(userDetails: UserDetails) {
+    const { username, email, roles, auth_id } = userDetails;
+
+    const userData = {
+      username,
+      email,
+      roles,
+      auth_id
+    };
+
+    const saveUserData = await this.databaseService.users.create({
+      data: userData
+    });
+
+    return saveUserData;
   }
 }
