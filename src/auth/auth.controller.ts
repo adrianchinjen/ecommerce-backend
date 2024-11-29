4542d66c004efcaecbabc7bce0e65bea378ae872
adrianchinjen <adrian.chinjen28@gmail.com>
2024-11-29 14:13:10 +0800

    fix: fixed signin route without response when trying to access

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,23 +1,25 @@
-import { Body, Controller, Get, Post, Res, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Res, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto/createUser.dto';
 import { Response } from 'express';
-// import { Public } from './decorators/public.decorator';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth/')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('signup')
   async signup(@Body(ValidationPipe) signUpCredentials: CreateUserDto) {
     const user = await this.authService.userSignUp(signUpCredentials);
     return user;
   }
 
+  @Public()
   @Post('signin')
   async signin(@Body(ValidationPipe) signInCredentials: LoginUserDto, @Res() response: Response) {
-    const testing = await this.authService.userSignIn(signInCredentials, response);
-    return testing;
+    const userRequest = await this.authService.userSignIn(signInCredentials, response);
+    return response.status(HttpStatus.OK).json(userRequest);
   }
 
   @Get('fetchPeople')
